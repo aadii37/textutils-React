68634e38091c87e551be92d907f56ad1a6359f67
Aditya <adityasingh19.005@gmail.com>
2023-08-21 11:30:38 +0530

    completed events basic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,14 +2,24 @@ import React, { useState } from "react"
 // import PropTypes from "prop-types"
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter Text Hereee');
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+
+    const [text, setText] = useState('Enter Text Hereee2');
     return (
         <div>
             <h1>{props.heading} </h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert To UpperCase</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
         </div>
     )
 }
